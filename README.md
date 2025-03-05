55c424b751689a87b1540dbafc07ec05907a8bb8
xiaowhang <b13528723800@163.com>
2025-03-05 13:23:21 +0800

    🎉 init: First commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# VJudge 获取 OJ 提交脚本
+
+该项目是一个用 Python 编写的脚本，用于从 VJudge 批量获取账号在 OJ 的提交数据。项目使用虚拟环境 (venv) 管理依赖，并通过 .env 文件配置环境变量。
+
+## 已支持 OJ
+
+- CodeForces
+- AtCoder
+- 洛谷
+
+## 安装与使用
+
+### 1. 克隆仓库
+
+使用 Git 克隆项目到本地：
+
+```bash
+git clone https://github.com/xiaowhang/vjudge-submission-tracker.git
+cd vjudge-submission-tracker
+```
+
+### 2. 创建虚拟环境
+
+建议使用 Python 内置的 `venv` 模块创建虚拟环境：
+
+```bash
+python3 -m venv venv
+```
+
+### 3. 激活虚拟环境
+
+- **Windows：**
+
+  ```bash
+  venv\Scripts\activate
+  ```
+
+- **macOS/Linux：**
+
+  ```bash
+  source venv/bin/activate
+  ```
+
+### 4. 安装依赖
+
+在虚拟环境激活状态下，使用 `pip` 安装项目依赖：
+
+```bash
+pip install -r requirements.txt
+```
+
+### 5. 配置环境变量
+
+项目使用 `.env` 文件存储环境变量信息。请按照以下步骤进行配置：
+
+1. 在项目根目录下创建 `.env` 文件（可以复制 `.env.example` 文件，如果仓库中提供了该示例）。
+2. 根据需要在 `.env` 文件中设置以下变量（示例配置）：
+
+   ```ini
+   # .env 示例
+   VJUDGE_COOKIE=your_vjudge_cookie_here
+   
+   CF_USER=your_CodeForces_username_here
+   ATC_USER=your_AtCoder_username_here
+   ```
+
+> 在 [VJudge.net](https://vjudge.net/problem) 中按 F12 打开开发者工具（DevTools），在控制台（Console）输入 `document.cookie` 即可获取所需的 cookie
+
+### 6. 配置
+
+在 [VJudge.net](https://vjudge.net/problem) 中给每个平台的题目添加账号信息：
+
+- [AtCoder-abc123_a](https://vjudge.net/problem/AtCoder-abc123_a)
+- [CodeForces-1A](https://vjudge.net/problem/CodeForces-1A)
+- [洛谷-P1001](https://vjudge.net/problem/洛谷-P1001)
+
+> 依次点击 `Submit`、`Submit by: Archive`、`Update`，填入对应的值。
+> 参考 [Submit with your own account](https://vjudge.net/article/2790)。
+
+### 7. 获取洛谷题目数据
+
+因洛谷没有 API 获取帐号提交的详细信息，故需手动添加通过的题号：
+
+在洛谷的个人主页中，点击 练习 可以查看已通过的题目。
+
+在 luogu 文件夹下新建 `problems.txt` 文件，将题目编号直接复制粘贴进去，格式见 `luogu/problems.example.txt` 文件（普及− 等信息可有可无）。
+
+### 8. 运行脚本
+
+在虚拟环境中运行主程序：
+
+```bash
+python main.py
+```
+
+## 贡献
+
+欢迎对本项目进行贡献。如果你有任何建议、问题或想提交 bug 修复，请提交 issue 或 pull request。
---
+++ main.py
@@ -0,0 +1,182 @@
+import copy
+import json
+import logging
+import os
+
+import dotenv
+import requests
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def read_lines(filename):
+    with open(filename, "r") as file:
+        return file.read().splitlines()
+
+
+def write_lines(filename, lines):
+    with open(filename, "w") as file:
+        file.write("\n".join(lines))
+
+
+def read_json(filename):
+    with open(filename, "r") as file:
+        return json.load(file)
+
+
+def write_json(filename, data):
+    with open(filename, "w") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+class Vjudge:
+    DEFAULT_DATA = {
+        "method": "2",
+        "language": None,  # 语言 ID
+        "open": "1",  # 是否公开代码
+        "source": "",
+        "oj": None,  # OJ 平台
+        "probNum": None,  # 题目编号
+    }
+
+    SUBMIT_URL = "https://vjudge.net/problem/submit"
+
+    def __init__(self):
+        dotenv.load_dotenv()
+        self.cookies = dict(item.split("=", 1) for item in os.getenv("VJUDGE_COOKIE").split("; "))
+        self.oj_config = {
+            "atcoder": {"language": "5001", "oj": "AtCoder"},
+            "codeforces": {"language": "91", "oj": "CodeForces"},
+            "luogu": {"language": "27", "oj": "洛谷"},
+        }
+
+    def update_problems(self):
+        logging.info("开始更新做题信息")
+
+        self.fetch("atcoder")
+        self.fetch("codeforces")
+        self.fetch("luogu")
+
+    def fetch(self, oj_name):
+        if not os.path.exists(oj_name):
+            os.mkdir(oj_name)
+        os.chdir(oj_name)
+
+        if oj_name == "atcoder":
+            self.get_ATC_problem()
+        elif oj_name == "codeforces":
+            self.get_CF_problem()
+        elif oj_name == "luogu":
+            self.get_LG_problem()
+
+        logging.info(f"开始提交 {oj_name} 做题信息")
+
+        problems = read_lines("problems.txt")
+        try:
+            succ = read_json("success_problems.json")
+        except FileNotFoundError:
+            succ = {}
+
+        for problem in problems:
+            if problem in succ and "success" in succ[problem] and succ[problem]["success"]:
+                continue
+
+            if problem in succ and "error" in succ[problem] and succ[problem]["error"] == "No recent submissions found":
+                continue
+
+            data = copy.deepcopy(self.DEFAULT_DATA)
+            for key, value in self.oj_config[oj_name].items():
+                data[key] = value
+            data["probNum"] = problem
+
+            if oj_name == "codeforces" and len(problem) > 6:
+                data["oj"] = "Gym"
+
+            response = requests.post(self.SUBMIT_URL, data=data, cookies=self.cookies)
+
+            if response.status_code == 200:
+                succ[problem] = json.loads(response.text)
+                logging.info(f"提交 {data['oj']}-{data['probNum']} 成功 {response.text}")
+
+                write_json("success_problems.json", succ)
+            else:
+                logging.error(f"提交 {data['oj']}-{data['probNum']} 失败, {response.status_code}")
+        os.chdir("..")
+
+    def get_ATC_problem(self):
+        logging.info("获取 AtCoder 题目信息")
+        url = "https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions"
+
+        try:
+            submissions = read_json("submissions.json")
+        except FileNotFoundError:
+            submissions = []
+
+        while True:
+            try:
+                last_time = submissions[-1]["epoch_second"] + 1
+            except IndexError:
+                last_time = 0
+            params = {"user": os.getenv("ATC_USER"), "from_second": last_time}
+
+            # 获取最新提交记录
+            response = requests.get(url, params=params).json()
+            submissions.extend(response)
+
+            if response == []:
+                break
+
+        write_json("submissions.json", submissions)
+
+        # 得到所有题目编号
+        try:
+            problems = set(read_lines("problems.txt"))
+        except FileNotFoundError:
+            problems = set()
+
+        for item in submissions:
+            problems.add(item["problem_id"])
+
+        write_lines("problems.txt", list(problems))
+
+    def get_CF_problem(self):
+        logging.info("获取 Codeforces 题目信息")
+        url = "https://codeforces.com/api/user.status"
+        params = {"handle": os.getenv("CF_USER")}
+
+        response = requests.get(url, params=params).json()
+
+        write_json("submissions.json", response)
+
+        try:
+            problems = set(read_lines("problems.txt"))
+        except FileNotFoundError:
+            problems = set()
+
+        submissions = response["result"]
+
+        for item in submissions:
+            problems.add(str(item["problem"]["contestId"]) + item["problem"]["index"])
+
+        write_lines("problems.txt", list(problems))
+
+    def get_LG_problem(self):
+        logging.info("获取 Luogu 题目信息")
+
+        try:
+            problems = read_lines("problems.txt")
+        except FileNotFoundError:
+            problems = []
+
+        for item in ["暂无评定", "入门", "普及−", "普及/提高−", "普及+/提高", "提高+/省选−", "省选/NOI−", "NOI/NOI+/CTSC"]:
+            if item in problems:
+                problems.remove(item)  # 删除无效信息
+
+        write_lines("problems.txt", problems)
+
+
+if __name__ == "__main__":
+    vju = Vjudge()
+    vju.update_problems()
